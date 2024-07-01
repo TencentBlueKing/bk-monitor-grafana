17658ff099410d501d624193535c17b08d7748d7
liangling <liangling@tencent.com>
2024-07-01 16:43:37 +0800

    feat: 去除GeoMap插件

--- packages/grafana-data/src/datetime/timezones.ts
+++ packages/grafana-data/src/datetime/timezones.ts
@@ -387,7 +387,6 @@ const countryByCode: Record<string, string> = {
   SE: 'Sweden',
   CH: 'Switzerland',
   SY: 'Syrian Arab Republic',
-  TW: 'Taiwan',
   TJ: 'Tajikistan',
   TZ: 'Tanzania',
   TH: 'Thailand',
